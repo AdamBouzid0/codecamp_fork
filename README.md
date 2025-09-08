bbdc49ccb4b57e713304f83e90fd55ed99603a6c
Mélina <melina.wang@imt-atlantique.net>
2025-09-08 22:06:26 +0200

    ajout readme et réorganisation des codes

---
+++ README.md
@@ -0,0 +1,28 @@
+# Auteurs (Groupe 4)
+
+- Rayene ABBASSI
+- Amira BALTI
+- Adam BOUZID
+- Luis RODRIGUES DE OLIVEIRA
+- Mélina WANG
+
+# Objectif du code
+
+Dans le cadre des séances *codecamp*, nous avions pour mission de produire en équipe un logiciel simple de gestion de tâches (interaction en ligne de commande dans un terminal : CLI). Le point de départ est commun à tous les groupes et consiste à réaliser les commandes d'ajout, de modification et de suppression de tâches sachant qu'une tâche a une description et un identifiant donné à sa création.
+
+# Fonctionnement des codes 
+
+- **core.py** : fichier dans lequel sont implémentées les fonctions
+- **commands.py** : correspond au fichier de commandes
+- **options.py** : code le parseur
+- **main.py** : code principal permettant de tester notre logiciel
+
+# Avancée du projet
+
+
+
+# Problèmes connus
+
+
+
+# Utilisation de l'IA
---
+++ codes/options.py
@@ -0,0 +1,25 @@
+import argparse
+
+def create_parser(): 
+    # Création du parseur de ligne de commande
+    parser = argparse.ArgumentParser(description='Simple task manager')
+    # Ajout d’un argument positionnel (le fichier contenant les tâches)
+    parser.add_argument('file', help='The tasks file')
+    # Ajout d’un sous-parseur pour les sous-commandes
+    subparsers = parser.add_subparsers(help='The commands to manage orders', dest='command', required=True)
+    
+    # Création du parseur pour la commande add
+    parser_add = subparsers.add_parser('add', help='Add a new task. The rest of the command line is used for the task details, the default being "no details".')
+    parser_add.add_argument('details', nargs='*', default="no details",
+    help="task details")
+    # Création du parseur pour la commande modify
+    parser_modify = subparsers.add_parser('modify',help='Modify a task given its id. The rest of the command line is used for the task details, the default being "no details"')
+    parser_modify.add_argument('id', help="the task id")
+    parser_modify.add_argument('details', nargs='*', default="no details",
+    help="the new details")
+    # Création du parseur pour la commande rm
+    parser_rm = subparsers.add_parser('rm', help='Remove an task given its id')
+    parser_rm.add_argument('id', help="the task id")
+    # Création du parseur pour la commande show
+    parser_show = subparsers.add_parser('show', help='Show the tasks')
+    return parser
